c50afa9191226a4eb0eca4a09842c4f89e3e52d9
Daniel Earley <danielse08@gmail.com>
2022-04-06 19:37:21 -0400

    functions challenge

--- week 6/challenges.py
+++ week 6/challenges.py
@@ -8,4 +8,20 @@
 # for letter in word:
 # 	print(letter)
 
-# def findVowels(word):
+def findVowels(word):
+	vowels ={"a":0, "e":0, "i":0, "o":0, "u":0}
+	for letter in word:
+		if letter == 'a':
+			vowels["a"] += 1
+		elif letter == "e":
+			vowels["e"] += 1
+		elif letter == "i":
+			vowels["i"] += 1
+		elif letter == "o":
+			vowels["o"] += 1
+		elif letter == "u":
+			vowels["u"] += 1
+	
+	return vowels
+
+print(findVowels("Hello"))
